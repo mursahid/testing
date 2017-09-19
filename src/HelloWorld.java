7e0ab03ee925f05c9ec92854fea6be636325d194
mursahid <sahidm24@gmail.com>
2017-09-19 16:13:52 +0700

    diganti Hai Hai Hai

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main (String [] args) {
-		System.out.println("Hello World again");
+		System.out.println("Hai Hai Hai World again");
 		System.out.println("Hai!");
 	}
 }
